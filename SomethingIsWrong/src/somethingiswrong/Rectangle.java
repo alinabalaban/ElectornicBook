d0d7ce68abfdd662c26eba8b8627bb163f172e56
alinabalaban <alina_irina_b@yahoo.com>
2016-03-25 22:43:37 +0200

    tema

---
+++ SomethingIsWrong/src/somethingiswrong/Rectangle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package somethingiswrong;
+
+/**
+ *
+ * @author Alina
+ */
+public class Rectangle {
+    public void area(){
+        int width=40;
+        int height=50;
+        System.out.println("myRect's area is "+ width*height);
+    }
+    
+}
---
+++ SomethingIsWrong/src/somethingiswrong/SomethingIsWrong.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package somethingiswrong;
+
+/**
+ *
+ * @author Alina
+ */
+public class SomethingIsWrong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Rectangle myRect=new Rectangle();
+        myRect.area();
+
+    }
+    
+}
