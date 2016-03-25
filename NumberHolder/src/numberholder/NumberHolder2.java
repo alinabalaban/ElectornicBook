f8ed8be9bce339735dc57a18b2d003971d78831e
alinabalaban <alina_irina_b@yahoo.com>
2016-03-25 22:43:30 +0200

    tema

---
+++ NumberHolder/src/numberholder/NumberHolder2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numberholder;
+
+/**
+ *
+ * @author Alina
+ */
+public class NumberHolder2 {
+    public int anInt=2;
+    public float aFloat=3;
+    public void display(){
+    anInt=0;
+    aFloat=3;
+    System.out.println("anInt is "+anInt+"aFloat is "+ aFloat);
+    }
+    
+}
